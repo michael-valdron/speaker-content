5f44b1958435a4f662b8ba9b555de56bfdc5cac3
Michael Valdron <michael.valdron@gmail.com>
2021-11-16 23:28:51 -0500

    Converted into lein project.

---
+++ ontariotech2021-csci3055u/leetcode/project.clj
@@ -0,0 +1,5 @@
+(defproject leetcode "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns leetcode.core}
+  :profiles {:kadanes {:repl-options {:init-ns leetcode.kadanes}}
+             :inverse-tree {:repl-options {:init-ns leetcode.inverse-tree}}})
